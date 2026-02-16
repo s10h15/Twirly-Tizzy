f95c72b9bb1cfcea83719907525eb937ab66cf47
s10h15 <algodoo135@gmail.com>
2026-02-17 03:03:50 +0600

    Complete repository reset and file path fix

--- Canvas/draw.js
+++
@@ -1,4 +0,0 @@
-export function clearCanvas(context, canvasElement) {
-	context.fillStyle = "black";
-	context.fillRect(0, 0, canvasElement.width, canvasElement.height);
-}
--- Helpers/textures.js
+++
@@ -1,7 +0,0 @@
-export function createTexture(width, height, drawCallback) {
-    const offscreen = new OffscreenCanvas(width, height);
-    const ctx = offscreen.getContext("2d");
-    ctx.imageSmoothingEnabled = false; 
-    drawCallback(ctx);
-    return offscreen; 
-}
